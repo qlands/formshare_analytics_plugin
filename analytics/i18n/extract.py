acb3ad4a4df0aa4621efdac1d80fdd4357a3935b
qlands <cquiros@qlands.com>
2022-12-19 08:52:21 -0500

    Replace function

--- analytics/i18n/extract.py
+++ analytics/i18n/extract.py
@@ -1,6 +1,6 @@
 from typing import Any
 from jinja2.ext import babel_extract
-from formshare.config.jinja_extensions import _get_extensions
+from formshare.config.jinja_extensions import get_extensions
 
 
 def extract_formshare(fileobj: Any, *args: Any, **kw: Any) -> Any:
@@ -9,7 +9,7 @@ def extract_formshare(fileobj: Any, *args: Any, **kw: Any) -> Any:
     :license: AGPL V3, see LICENSE for more details."""
     extensions = [
         ":".join([ext.__module__, ext.__name__]) if isinstance(ext, type) else ext
-        for ext in _get_extensions()
+        for ext in get_extensions()
     ]
     if "options" not in kw:
         kw["options"] = {}
